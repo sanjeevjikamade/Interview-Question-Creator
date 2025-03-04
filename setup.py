af5a71bdb0e21ff2d2048e8640db69cf990d4652
Sanjeev Jikamade <sanjeev.jikamade@lsdigital.com>
2025-03-04 16:42:06 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Generative AI Project",
+    version= '0.0.1',
+    author="Sanjeev Jikamade",
+    author_email="sanjeev.jikamade@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
